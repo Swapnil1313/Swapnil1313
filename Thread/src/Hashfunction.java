04f580f6b19f5f5f1688f7d83df4774b29757db7
kalya <kalya@LAPTOP-811GSGNJ>
2022-03-31 19:47:05 +0530

    Commit Hashset

---
+++ Thread/src/Hashfunction.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.util.*;
+
+class Hashfunction
+{
+
+	public static void main(String[] args)
+	{
+		HashSet<Integer> hashnum = new HashSet<Integer>();
+		hashnum.add(10);
+		hashnum.add(30);
+		hashnum.add(40);
+		hashnum.add(20);
+		
+		 
+		System.out.println(hashnum);
+	}
+}
